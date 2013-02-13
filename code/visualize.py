ab62d10b10b0a5895604f43198062d5f7f7509d9
James Fairbanks <james.fairbanks@gatech.edu>
2013-02-13 16:43:06 -0500

    refactoring the bc analysis into functions that capture data dependence

--- code/visualize.py
+++ code/visualize.py
@@ -97,7 +97,7 @@ def normalize(seq, prob=False):
         m = np.max(seq)
         return seq/m
 
-def compare_kings_seq(seq, start_col=0, end_col=-1,
+def compare_kings_seq(rf, seq, start_col=0, end_col=-1,
                   ascending=False, plot=False, **kwargs):
     ''' Compare the entries at the top at the start to
     the entries at the top at the end.
@@ -178,7 +178,7 @@ def describe_vertex(df, lf, rf, v, plot=False, **kwargs):
 
 def distribution_describe(df, colindex=None, plot=False,
                           transform=None, **kwargs):
-    """Makes a simple description assuming lognormal
+    """Makes a simple description using pandas.describe
 
     Arguments:
     - `df`:
@@ -198,116 +198,151 @@ def distribution_describe(df, colindex=None, plot=False,
         desc.T.plot(**kwargs)
     return desc
 
+def quantile_analysis(df, vertices, quants, timer=None, plot=False, **kwargs):
+    """show how the quantiles of the distribution change over time
+
+    Arguments:
+    - `df`:
+    - `vertices`:
+    - `quants`:
+    - `timer`:
+    - `plot`
+    - `**kwargs`: matplotlib kwargs
+    """
+    # Quantile analysis
+    if timer:
+        timer.tic('quants')
+    quants = [.1, .25, .5, .75, .9, .95, .99, 1]
+    tf = df.ix[vertices]
+    qf = pd.DataFrame({q:tf.quantile(q) for q in quants})
+    if plot:
+        qf.plot(**kwargs)
+    if timer:
+        timer.toc('quants')
+    return qf
+
+def auto_correlate(df, columns='diff'):
+    """makes a autocorrelation plot of the columns
+
+    Arguments:
+    - `df`:
+    - `columns`:
+    """
+    # TODO: autocorrelation
+    correlogram = pd.tools.plotting.autocorrelation_plot
+    plt.figure()
+    ax = correlogram(df[columns].dropna())
+    return ax
+
+def run_bc_analysis(df, timer):
+    """
+
+    Arguments:
+    - `df`:
+    """
+    TARGETS_INITIAL = df[1].dropna().index #initial vertex set
+    #count how many entries are not defined,
+    #vertices who have not appeared yet or have fallen below epsilon
+    num_nans = df.apply(np.isnan).sum()
+    print("The number of NANs by timestep:\n", num_nans)
+    #compute the ranks for each vertex at each timethod='minm, na_option='top'e step
+    timer.tic('ranking')
+    rf = df.rank(method='min', na_option='top')
+    timer.toc('ranking')
+
+    # TODO: demonstrate that the leaders at the beginning are the leaders at the end
+    compare_kings_seq(rf, [10, 30, 300], start_col=5)
+    #the similarity of the top at begin and end is large and then falls very quickly
+    # TODO: what happens to the people who start at the bottom.
+    compare_kings_seq(rf, [100, 300, 3000], start_col=5, ascending=True)
+    smallfish = df[df[21] < 0.1][21].index
+    # TODO: can we find anyone making a smooth climb to the top
+
+    timer.tic('density')
+    distribution_describe(df, df.columns[::10], transform=np.log,
+                          plot=True, title='changing distribution of log(bc)')
+    timer.toc('density')
+    # Tracing some vertices though time either random or seeded
+
+    if TARGETSV:
+        targets = random_targets(rf, 1, 4, pool=TARGETSV)[0].columns
+    else:
+        # sample from the set of vertices that have ever been above a high quantile
+        q=.500
+        qmedians = df.quantile(q)
+        topq = df[df > qmedians]
+        targets = random_targets(rf, 1, 6, pool=topq.index)[0].columns
+
+    numzeros = (rf[rf<=1].count())
+    numzeros.plot(label='rank($\epsilon)$')
+    v = targets[0]
+    lf = df.apply(np.log)
+    summary = describe_vertex(df, lf, rf, v, plot=True,
+                              title='profile of vertex %d'%v,)
+    difftarg = lf.ix[targets].T.diff()
+    diffax = difftarg.plot(title='diffs in log '+ KERNEL_NAME)
+    diffax.set_xlabel('batch')
+    diffax.set_ylabel('change')
+    plt.figure()
+    return targets, summary
+
+def derivative_analysis(lf, vertices, timer=None):
+    """
+
+    Arguments:
+    - `lf`:
+    - `vertices`:
+    """
+
+    ## DERIVATIVES of log BC
+    if timer:
+        timer.tic('diff')
+    # show the derivative using logs to account for exponential dist of BC
+    diffs = lf.T.diff().T
+
+    # TODO: find the peaks of the derivative, these are the jumps in BC
+    if timer:
+        timer.toc('diff')
+
+    # TODO: find significant jumps and count them
+    # TODO: find the distribution of the jumps over time.
+    #peaks in the differences
+    peakslocs = diffs.replace(np.nan, -100).T.apply(np.argmax)*STRIDE
+    ax = peakslocs.hist(bins=NSAMPLES/STRIDE,
+                   normed=False,)
+    ax.set_title('location of maximum change in bc')
+    ax.set_ylabel('count')
+    ax.set_xlabel('batch')
+    # TODO: why do some batches have so many vertices with peaks there
+    #david thinks these are connected component merges
+    # TODO: measure slopes as a function distance from rank(e)
+    print(timer)
+    # TODO: can we segment the lines into jumps and linear segments?
+    # the difference between the slope of val and rank lines is the attrition rate
+    #TODO: how many jumps does each vertex have
+    return peakslocs
+
 DATA_DIR = u'/shared/users/jfairbanks/sandyfull/'
 FIGUREPATH = u'/shared/users/jfairbanks/smisc.sandystudy/output/'
 NSAMPLES = 1000 #number of batches
 STRIDE = 10 #resolution of time in batches
 NTARGETS = 8 #number of series for the plots
 BINCOUNT = 50 #how many bins for histograms
-TARGETSV = [328, 457, 5056, 340807, 12859, 12860] #vertices that I picked by hand
+TARGETSV = []
+TARGETSV = [3784858, 2357671, 2975930, 359724, 2124973, 3732925,] #vertices that I picked by hand
 KERNEL_NAME = "bc"
 KERNEL_NAMES = ['bc', 'communities', 'components']
 timer = td.timedict()
+
 #run_lognormal_analysis(DATA_DIR, NSAMPLES, KERNEL_NAME)
+
 timer.tic('loading data')
 df = load_batches(DATA_DIR+ KERNEL_NAME+".%d.vec", range(1, NSAMPLES, STRIDE))
 timer.toc('loading data')
-#count how many entries are not defined,
-#vertices who have not appeared yet or have fallen below epsilon
-num_nans = df.apply(np.isnan).sum()
-print("The number of NANs by timestep:\n", num_nans)
-#initial vertex set
-TARGETSV = df[1].dropna().index
-#replace nans  with 0 to fix s#orting order
-timer.tic('replacing nans')
-#df = df.replace(np.nan, 0)
-timer.toc('replacing nans')
-
-#compute the ranks for each vertex at each timethod='minm, na_option='top'e step
-timer.tic('ranking')
-rf = df.rank(method='min', na_option='top')
-timer.toc('ranking')
-signal = rf.ix[TARGETSV]
-#print(signal)
-#signal.T.apply(normalize).plot()
-
-# TODO: show that the path to the end is jagged and jumpy
-#randframes = random_targets(rf, 1, 6)s
-
-# Quantile analysis
-timer.tic('quants')
-quants = [.1, .25, .5, .75, .9, .95, .99, 1]
-tf = df.ix[TARGETSV]
-#use both the initial vertices and the entire vertex set for quantiles
-#qf = pd.DataFrame({q:tf.quantile(q) for q in quants})
-# TODO: come back to the quantile analysis
-#qf = pd.DataFrame({q:df.quantile(q) for q in quants})
-#qf.plot(logy=True)
-timer.toc('quants')
-# TODO: demonstrate that the leaders at the beginning are the leaders at the end
-compare_kings_seq([10, 30, 300], start_col=5)
-#the similarity of the top at begin and end is large and then falls very quickly
-# TODO: what happens to the people who start at the bottom.
-compare_kings_seq([100, 300, 3000], start_col=5, ascending=True)
-smallfish = df[df[21] < 0.1][21].index
-# TODO: count the number of times a vertex appears in top n
-# TODO: can we find anyone making a smooth climb to the top
-#TODO: sample from the set of vertices that have ever been above a high quantile
-q=.500
-qmedians = df.quantile(q)
-topq = df[df > qmedians]
-targets = random_targets(rf, 1, 6, pool=topq.index)[0].columns
-
-numzeros = (rf[rf<=1].count())
-numzeros.plot(label='rank($\epsilon)$')
-nnz = rf[rf>1].count()
-timer.tic('diff')
-# show the derivative using logs to account for exponential dist of BC
-lf = df.apply(np.log)
-diffs = lf.T.diff().T
-difftarg = diffs.ix[targets].T
-lftarg = lf.ix[targets].T
-difftarg.plot(title='diffs in log '+ KERNEL_NAME)
-plt.figure()
-#TODO: what do the diffs look like en masse
-timer.tic('density')
-diffsums = diffs.sum().dropna()
-ax = diffsums.hist(bins=BINCOUNT, normed=True)
-diffsums.plot(kind='kde',
-              title='distribution of total change in log(bc)',)
-#diffs.unstack().dropna().plot(kind='kde', title='kde of diffs flattened')
-distribution_describe(df, df.columns[::10], transform=np.log,
-             plot=True, title='changing distribution of log(bc)')
-timer.toc('density')
-# percent change is kind of useless
-#lftarg.T.pct_change().T.plot(title='% change of diffs in log'+KERNEL_NAME)
-# TODO: find the peaks of the derivative, these are the jumps in BC
-w = lf.ix[targets].T
-pos = w[w.diff()>0]
-timer.toc('diff')
-#plt.figure()
-v = targets[0]
-summary = describe_vertex(df, lf, rf, v, plot=True, title='profile of %d'%v)
-print(summary.tail(40))
-
-# TODO: find the distribution of the jumps over time.
-#peaks in the differences
-plt.figure()
-peakslocs = diffs.replace(np.nan, -100).T.apply(np.argmax)*STRIDE
-peakslocs.hist(bins=NSAMPLES/STRIDE,
-               normed=True,)
-#plt.set_title('location of maximum change')
-# TODO: why do some batches have so many vertices with peaks there
-# TODO: measure slopes as a function distance from rank(e)
-print(timer)
-plt.show()
-# TODO: can we segment the lines into jumps and linear segments?
-# the difference between the slope of val and rank lines is the attrition rate
-
-#TODO: how many jumps does each vertex have
-
-# TODO: autocorrelation
-correlogram = pd.tools.plotting.autocorrelation_plot
-plt.figure()
-correlogram(summary['diff'].dropna())
+#run_bc_analysis(df, timer)
+peakslocs = derivative_analysis(df.apply(np.log), None, timer)
+peakloc_counts = peakslocs.value_counts()
+peakpeaks = peakslocs.value_counts()[:3]
+cf = load_batches(DATA_DIR+'components.%d.csv', range(1,999,10))
+cf[[421,431,441]].dropna(how='all')
 plt.show()
