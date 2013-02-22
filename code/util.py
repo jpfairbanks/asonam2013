b656da54095ac90bb3f97cb9ce2daf0adfdf09a5
James Fairbanks <jfairbanks@hpc18.cc.gt.atl.ga.us>
2013-02-22 08:15:00 -0500

    move normalizing a series to util

--- code/util.py
+++ code/util.py
@@ -6,3 +6,16 @@ def jaccard(A, B):
     num = (len(A.intersection(B))+0.0)
     denom = (len(A.union(B))+0.0)
     return num/denom
+
+def normalize(seq, prob=False):
+    ''' Takes a sequence and normalizes it
+
+    Arguments:
+    - seq
+    - prob: if true then it will normalize to sum to 1
+    '''
+    if prob:
+        return seq/np.sum(seq)
+    else:
+        m = np.max(seq)
+        return seq/m
