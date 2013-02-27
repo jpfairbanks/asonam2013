93142e0d7a591b728593b7b176b1624c25963e2e
James Fairbanks <jfairbanks@hpc18.cc.gt.atl.ga.us>
2013-02-27 16:35:24 -0500

    add documentation for paper_figures

--- code/paper_figures.py
+++ code/paper_figures.py
@@ -1,3 +1,17 @@
+""" This module uses the plotting module (written by James Fairbanks) in order
+to make the publication ready figures for the paper about this research. The
+goal is to run a script that will generate all of the figures with titles,
+captions, and axis labels. This will allow the maximum agility in publication.
+
+These functions should just call functions from plotting.py with the propoer
+arguments for the paper. Then annotate the resulting plots and save them to disk
+with the filenames that are in the latex document for includegraphics.
+
+Any function that makes a plot should be in the plotting module written to be
+reused and the specialized in this module.
+
+
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as stats
