36957fe79ab74541ae8ff93b196bcb4a3ea175a9
James Fairbanks <jfairbanks@hpc18.cc.gt.atl.ga.us>
2013-03-08 08:50:04 -0500

    Correlation of a kernel with changing gaps

--- code/kernel_analysis.py
+++ code/kernel_analysis.py
@@ -52,3 +52,27 @@ def exec_crosstabs(df,timestamps,epsilon):
     print(marg_vals)
     marg_ranks = (ct+0.0)/ct.sum(axis=0)
     return ct, marg_vals, marg_ranks
+
+def rhotk(df,sample_times, display_starts, method='pearson'):
+    """ compute the correlation going forward. This corresponds to the
+    rho(t,t+k) for t+k in sample_times. We only return the frame containing
+    columns for each t in display_starts.
+
+    Arguments:
+    - `df`: the data from must contain sample_times as columns
+    - `sample_times`: the points in time at which to compute the correlations
+    - `display_starts`: must be a subset of sample_times
+    - `method`: pearson, spearman, kendall. pearson is the fastest and default
+
+    Returns:
+    -`rhos`: a dataframe containing a column for each starting point and the
+             rows are all t+k in sample_times. You can call plot on this to
+             visualize it quickly
+
+    Note:
+    pearson correlation is the fastest but only captures linear correlation.
+    Spearman's correlation is a rank correlation and so is for any monotonic
+    relationship.
+    """
+    rhos = df[sample_times].corr(method=method)[display_starts]
+    return rhos
