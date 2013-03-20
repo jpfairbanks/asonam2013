53081b6f7b794cd310bcbb749ee187b62307b0fb
James Fairbanks <jfairbanks@hpc18.cc.gt.atl.ga.us>
2013-03-20 11:07:42 -0400

    add wrapper for multivariate normal that preserves pandas labels

--- code/util.py
+++ code/util.py
@@ -2,6 +2,21 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
+def mvnrand(covariance,sample_size=10000, mean=None):
+    """	
+    Draw from a multivariate gaussian preserving the axes labels on the 
+    covariance matrix.
+    Arguments:
+    - `covariance`:
+    - `sample_size`:
+    - `mean`:
+    """
+    if mean is None:
+        mean = np.zeros(covariance.shape[0])
+        data = rand.multivariate_normal(mean, covariance, sample_size)
+    return pd.DataFrame(data,columns=covariance.columns)
+
+
 def jaccard(A, B):
     num = (len(A.intersection(B))+0.0)
     denom = (len(A.union(B))+0.0)
