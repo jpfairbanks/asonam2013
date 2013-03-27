f65868adadd5a7e141c600339fea2ee9e8aaea38
James Fairbanks <jfairbanks@hpc18.cc.gt.atl.ga.us>
2013-03-27 15:28:51 -0400

    regression test for betweenness_centrality.py just calls it with all command arguments seperately

---
+++ code/test_bc.sh
@@ -0,0 +1,8 @@
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py -h" && echo "passed help"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py -s" && echo "passed static"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py -t" && echo "passed temporal"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py --traces" && echo "passed traces"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py -d" && echo "passed derivative"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py -r" && echo "passed correlation"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py -x" && echo "passed crosstab"
+~/.local/bin/ipython ipython -c "run betweenness_centrality.py --scatter" && echo "passed scatter"
